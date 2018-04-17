8526d710ffe4db122be9fff5edbbfc7ad67a0cdf
vmitache <v.mitache@gmail.com>
2018-04-17 12:57:18 +0300

    First CDI Servlet / Weld example - to be fixed !!!

---
+++ web/src/main/java/hotel/controllers/FirstServletWithInjection.java
@@ -0,0 +1,41 @@
+package hotel.controllers;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mhotel.model.Address;
+
+/**
+ * Servlet implementation class FirstServletWithInjection
+ */
+@WebServlet("/fsi")
+public class FirstServletWithInjection extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Address mAddr;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServletWithInjection() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("addr: ").append(mAddr.toString());
+	}
+
+	
+}
