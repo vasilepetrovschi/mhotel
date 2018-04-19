7a8add27432f9a53d01369064c5af3476dbdfcc6
vmitache <v.mitache@gmail.com>
2018-04-19 12:58:41 +0300

    JPA cut 1

---
+++ model/src/main/java/mhotel/model/Car.java
@@ -0,0 +1,107 @@
+package mhotel.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cars",schema="test")
+public class Car {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "car_maker")
+	private String mMaker;
+	@Column(name = "car_model")
+	private String mModel;
+	@Column(name = "car_year")
+	private int mYear;
+	@Column(name = "color")
+	private String mColor;
+	@Column(name = "milleage")
+	private int mMileage;
+
+	public String getMaker() {
+		return mMaker;
+	}
+
+	public void setMaker(String pMaker) {
+		mMaker = pMaker;
+	}
+
+	public String getModel() {
+		return mModel;
+	}
+
+	public void setModel(String pModel) {
+		mModel = pModel;
+	}
+
+	public int getYear() {
+		return mYear;
+	}
+
+	public void setYear(int pYear) {
+		mYear = pYear;
+	}
+
+	public String getColor() {
+		return mColor;
+	}
+
+	public void setColor(String pColor) {
+		mColor = pColor;
+	}
+
+	public int getMileage() {
+		return mMileage;
+	}
+
+	public void setMileage(int pMileage) {
+		mMileage = pMileage;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long pId) {
+		id = pId;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [id=" + id + ", mMaker=" + mMaker + ", mModel=" + mModel + ", mYear=" + mYear + ", mColor=" + mColor
+				+ ", mMileage=" + mMileage + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
